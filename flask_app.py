6bb930a53691ce28c804e778d6587714d1ae0f1b
Chennammal S <104779488+chennammal15saravanan@users.noreply.github.com>
2025-07-02 15:28:49 +0530

    flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import flask, render_template, request, redirect, url_for, session, jsonify
 
 from flask_cors import CORS
 from supabase import create_client, Client
